e691c18732da5a328f686f5a5be567837ad7bfdb
vgrillot <grivin@gmail.com>
2018-07-28 20:55:55 +0200

    add screens

--- wop/urls.py
+++ wop/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from wop import views
 
 urlpatterns = [
-    path('', views.screen_setup_list_view, name='index'),
+    # path('', views.screen_setup_list_view, name='index'),
+    path('config/', views.config_view, name='config'),
+    path('', views.screen_setup_list_view, name='screen_setup_list'),
     path('screen_setup/<int:screen_setup_id>/', views.screen_setup_detail_view, name='screen_setup_detail'),
     path('screen/<int:screen_id>/', views.screen_detail_view, name='screen_detail'),
+    path('image_repo/', views.repo_list_view, name='image_repo_list'),
+    path('image_repo/<int:image_repo_id>/', views.image_repo_detail_view, name='image_repo_detail'),
 ]
--- wop/views.py
+++ wop/views.py
@@ -6,7 +6,22 @@ def screen_setup_list_view(request):
     context = {
         'screen_setups': ScreenSetup.objects.all()
     }
-    return render(request, 'index.html', context)
+    return render(request, 'screen_setup_list.html', context)
+
+
+def repo_list_view(request):
+    context = {
+        'image_repos': ImageRepo.objects.all()
+    }
+    return render(request, 'image_repo_list.html', context)
+
+
+def image_repo_detail_view(request, image_repo_id):
+    image_repo = get_object_or_404(ImageRepo, pk=image_repo_id)
+    context = {
+        'image_repo': image_repo
+    }
+    return render(request, 'image_repo.html', context)
 
 
 def screen_setup_detail_view(request, screen_setup_id):
@@ -23,3 +38,8 @@ def screen_detail_view(request, screen_id):
         's': s
     }
     return render(request, 'screen.html', context)
+
+
+def config_view(request):
+    return render(request, 'config.html')
+
