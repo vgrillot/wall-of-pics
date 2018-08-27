567b7cf2388b6394c33537c67bd701ef66c4cfb7
vgrillot <grivin@gmail.com>
2018-08-27 23:36:52 +0200

    image pre analysis

--- wop/tools/scan_repo.py
+++ wop/tools/scan_repo.py
@@ -1,16 +1,19 @@
 """
 tool unit to scan repository and update database
 """
+from functools import reduce
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.timezone import make_aware
 from wop import models
+import PIL.Image
+import PIL.ImageStat
 
-
-#!!180826:Creation
 
 import os
 import glob
 from datetime import datetime
+
 
 
 def scan_repo(image_repo:models.ImageRepo):
@@ -35,6 +38,48 @@ def scan_repo(image_repo:models.ImageRepo):
             image.key = hex(abs(hash(image.path)))[2:]  # get unique identifier
             image.save()
             new_images.append(image)
+
+            infos = image_analysis(image)  # TODO:for test purposes
+
         if j > 5:
             break # TODO: for testing, do only 5 scan (quicker, repeatable without db delete)
     return new_images
+
+
+def image_analysis(image:models.Image):
+    """read file information"""
+    file = PIL.Image.open(image.path)
+
+    image.width = file.size[0]
+    image.height = file.size[1]
+    image.ratio = image.width / image.height
+    image.b_w = isblack(file)
+    image.analysed = True
+    image.save()
+
+
+def isblack(file):
+    """check if filename is a black & white photo
+    """
+    # from https://www.programcreek.com/python/example/92040/PIL.ImageStat.Stat
+    MONOCHROMATIC_MAX_VARIANCE = 0.005
+    COLOR = 1000
+    MAYBE_COLOR = 200
+    v = PIL.ImageStat.Stat(file).var
+    is_monochromatic = reduce(lambda x, y: x and y < MONOCHROMATIC_MAX_VARIANCE, v, True)
+    if is_monochromatic:
+        return True
+    else:
+        if len(v) == 3:
+            maxmin = abs(max(v) - min(v))
+            if maxmin > COLOR:
+                return False
+            elif maxmin > MAYBE_COLOR:
+                return False
+            else:
+                return True
+        elif len(v)==1:
+            return True
+        else:
+            #don't know
+            return None
